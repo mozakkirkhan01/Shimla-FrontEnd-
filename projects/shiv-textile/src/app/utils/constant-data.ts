f47a5a39a7d79e3ba91957d7ddb8d223a8fceee5
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2026-09-05 15:58:00 +0530

    Adds MonthList to ConstantData for improved date handling

--- projects/shiv-textile/src/app/utils/constant-data.ts
+++ projects/shiv-textile/src/app/utils/constant-data.ts
@@ -2,5 +2,19 @@ export class ConstantData {
     public static SuccessMessage = "Success";
     public static PageSizes = [10, 20, 100, 200, 500, 1000, 2000, 5000];
     public static StatusList = [{ Key: 1, Value: "Active" }, { Key: 2, Value: "Inactive" }];
+    public static MonthList = [
+    { value: 1, name: 'January' },
+    { value: 2, name: 'February' },
+    { value: 3, name: 'March' },
+    { value: 4, name: 'April' },
+    { value: 5, name: 'May' },
+    { value: 6, name: 'June' },
+    { value: 7, name: 'July' },
+    { value: 8, name: 'August' },
+    { value: 9, name: 'September' },
+    { value: 10, name: 'October' },
+    { value: 11, name: 'November' },
+    { value: 12, name: 'December' }
+  ];
     public static BoolList = [{ Key: true, Value: "Yes" }, { Key: false, Value: "No" }];
 }
